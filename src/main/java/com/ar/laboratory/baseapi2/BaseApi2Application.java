9061f155f34db83f1c18909d7eeabe99dc7f6d9e
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2026-02-02 19:00:43 -0300

    Initial commit

---
+++ src/main/java/com/ar/laboratory/baseapi2/BaseApi2Application.java
@@ -0,0 +1,13 @@
+package com.ar.laboratory.baseapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseApi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApi2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ar/laboratory/baseapi2/BaseApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.laboratory.baseapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseApi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
